ccff0e3c56ec0619d6bea6de32f92a3b20ba29de
localice2021 <94673672+localice2021@users.noreply.github.com>
2021-11-26 19:58:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,9 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# Add sources
+svn co https://github.com/coolsnowwolf/lede.git/trunk/package/lean/luci-app-vsftpd /package/openwrt-packages
+svn co https://github.com/coolsnowwolf/lede.git/trunk/package/lean/vsftpd-alt /package/openwrt-packages
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
