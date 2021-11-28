3e779738a5a249e8d0bccd121a1222f6c569fe29
localice2021 <94673672+localice2021@users.noreply.github.com>
2021-11-28 10:46:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,8 @@
 # Add sources
 svn co https://github.com/coolsnowwolf/lede.git/trunk/package/lean/luci-app-vsftpd package/luci-app-vsftpd
 svn co https://github.com/coolsnowwolf/lede.git/trunk/package/lean/vsftpd-alt package/vsftpd-alt
-svn co https://github.com/kuoruan/openwrt-upx/trunk/upx tools/upx
+rm -rf tools
+svn co https://github.com/coolsnowwolf/lede/trunk/tools tools
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
