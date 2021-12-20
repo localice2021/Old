aae26d48eaf11859eec7f9158ad9e5bd2eaf79f5
localice2021 <94673672+localice2021@users.noreply.github.com>
2021-12-20 20:42:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i 's/\'OpenWrt\'/\'Phicomm\'/g' package/base-files/files/bin/config_generate
 #Modify ssid
-sed -i 's/OpenWrt/Phicomm/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/Dir05/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # Modify wireless
 # sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
